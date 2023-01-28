369dbd95cb2dff75ba28f4dfb1623c6edf8d3a89
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-28 18:25:57 -0300

    feat: Requisito 7.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -48,4 +48,11 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    filtered_job_industry = []
+
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_job_industry.append(job)
+
+    return filtered_job_industry
