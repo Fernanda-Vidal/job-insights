ab5bd8b7fa8637cd9bbd2e91dcb9dc5e229e85d5
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-28 18:17:46 -0300

    feat: Requisito 6.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -67,4 +67,11 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    filtered_jobs = []
+
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_jobs.append(job)
+    
+    return filtered_jobs
