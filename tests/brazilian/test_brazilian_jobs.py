6eb7689db473166a088a8f1fd01bcf60417e2668
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-29 19:08:56 -0300

    feat: Requisito 11.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+
+    list_dict = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for dict in list_dict:
+        assert dict.keys() == ("title", "salary", "type")
