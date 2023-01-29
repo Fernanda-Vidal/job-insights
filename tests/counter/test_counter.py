b480857b29bb5da3ea1bfc5df63dfa7f2b4d9ae7
Fernanda Vidal <fevidal.dev@gmail.com>
2023-01-29 18:42:23 -0300

    refactor: Lint

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,6 +1,6 @@
 from src.pre_built.counter import count_ocurrences
 
+
 def test_counter():
     assert (count_ocurrences(path="data/jobs.csv", word="Python") == 1639)
     assert (count_ocurrences(path="data/jobs.csv", word="Javascript") == 122)
-
